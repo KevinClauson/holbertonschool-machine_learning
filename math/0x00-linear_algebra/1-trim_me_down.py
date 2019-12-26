3730d57b3b8a6dbff284d8347e5bc5052c540ffd
KevinClauson <kevinclauson99@gmail.com>
2019-12-26 04:28:00 +0000

    fix indent 2

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -2,5 +2,5 @@
 x = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for arr in x:
-  the_middle.append(arr[2:4])
+    the_middle.append(arr[2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
