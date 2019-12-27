e1a3e0313f78c0b74a5279b334d448cdb2143dce
KevinClauson <kevinclauson99@gmail.com>
2019-12-27 23:59:28 +0000

    delete line

--- math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
+++ math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
@@ -2,7 +2,6 @@
 import numpy as np
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
-print(matrix.shape)
 mat1 = matrix[[int(matrix.shape[0]/2) - 1, int(matrix.shape[0]/2)], ]
 mat2 = matrix[:, [int(matrix.shape[1]/2) - 1, int(matrix.shape[1]/2)]]
 mat3 = matrix[-3:, -3:]
