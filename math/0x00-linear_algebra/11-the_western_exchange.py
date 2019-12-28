c3445c4dba51def2df836cff998ebcc678d15672
KevinClauson <kevinclauson99@gmail.com>
2019-12-28 04:19:59 +0000

    tranpose a matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""that transposes matrix"""
+
+
+def np_transpose(matrix):
+    """
+        using numpy to transpose matrix
+        matrix: numpy.ndarray 
+        Return: a new numpy.ndarray
+    """
+    return matrix.T
