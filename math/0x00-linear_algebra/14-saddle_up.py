8cc451ff284fa7dcbb3ebfff89d4b8b6e781da67
KevinClauson <kevinclauson99@gmail.com>
2019-12-28 05:28:46 +0000

    add mult

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Multiply two matrices
+        mat1: numpy.ndarray
+        mat2: numpy.ndarray
+        Return: mult array
+    """
+    return np.matmul(mat1, mat2)
