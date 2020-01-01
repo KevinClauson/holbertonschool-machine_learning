80793bae2407566b5eafce441ed369cc02beeb2b
KevinClauson <kevinclauson99@gmail.com>
2020-01-01 22:46:13 +0000

    add sum function

---
+++ math/0x01-calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Function that returns sum of squares"""
+
+
+def summation_i_squared(n):
+    """
+        Function that takes an integer and returns the square of all ints
+        from 1 to that integer and including that integer.
+        n: integer
+        Return: an integer of the sum
+    """
+    if not isinstance(n, int) or n < 0:
+        return None
+    return int((2*(n**3)+(3*(n**2))+n)/6)
