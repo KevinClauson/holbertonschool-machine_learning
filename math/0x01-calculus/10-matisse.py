cf8e88e02d88ed2b555fe5686c85a7ce8de33072
KevinClauson <kevinclauson99@gmail.com>
2020-01-02 00:32:26 +0000

    fix returnx

--- math/0x01-calculus/10-matisse.py
+++ math/0x01-calculus/10-matisse.py
@@ -8,10 +8,12 @@ def poly_derivative(poly):
         poly: is a list of coefficients representing a polynomial
         Return: a new list of coefficients for the derivative of polynomial
     """
-    if not poly or type(poly) is not list or len(poly) < 1 or type(poly[0]) is not int:
+    if type(poly) is not list or len(poly) < 1 or type(poly[0]) is not int:
         return None
     der = []
     for i, val in enumerate(poly):
         if i > 0:
             der.append(i * val)
+    if len(der) == 0:
+        der = [0]
     return der
