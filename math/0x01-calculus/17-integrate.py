913a4d514c823667ed0b31fe04951e1aa8f4d0a6
KevinClauson <kevinclauson99@gmail.com>
2020-01-03 05:58:46 +0000

    and integrals

---
+++ math/0x01-calculus/17-integrate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""that calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """
+        A function that calculated the integral of a polynomial
+        poly: is a list of coefficients representing a polynomial
+        C: is an integer representing the integration constant
+        Return: a new list of coefficients representing the integral
+        of the polynomial
+    """
+    integral = [C]
+    for i, val in enumerate(poly):
+        if val == 0:
+            integral.append(0)
+        else:
+            integral.append(val/(i + 1))
+    return integral
