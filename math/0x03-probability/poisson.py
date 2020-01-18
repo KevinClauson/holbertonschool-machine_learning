e220597c1a549cd39b170d4afc33102e688af01f
KevinClauson <kevinclauson99@gmail.com>
2020-01-18 22:50:06 +0000

    fix bugss

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -11,13 +11,14 @@ class Poisson:
             data: is a list of the data to be used to estimate the distribution
             lambtha: is the expected number of occurences in a given time frame
         """
-        if not data and lambtha <= 0:
-            raise ValueError('lambtha must be a positive value')
-        if data and type(data) is not list:
-            raise TypeError('data must be a list')
-        if data and len(data) < 2:
-            raise ValueError('data must contain multiple values')
+        if not data:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = lambtha/1.0
         if data:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
             self.lambtha = (sum(data) / len(data)) / 1.0
-        else:
-            self.lambtha = lambtha/1.0
